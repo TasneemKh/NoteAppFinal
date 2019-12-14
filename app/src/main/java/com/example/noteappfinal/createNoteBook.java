025041465616b44862ffc09954ec79ebebdc74e9
Tasneem Khrais <eng.khraistasneem@gmail.com>
2019-12-14 15:42:40 +0200

    "adding firebase"

---
+++ app/src/main/java/com/example/noteappfinal/createNoteBook.java
@@ -0,0 +1,14 @@
+package com.example.noteappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createNoteBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note_book);
+    }
+}
---
+++ app/src/main/java/com/example/noteappfinal/mainSlide.java
@@ -0,0 +1,14 @@
+package com.example.noteappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainSlide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_slide);
+    }
+}
